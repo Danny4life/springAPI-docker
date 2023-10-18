4452c32c101bccaba2ad83b25c01a64bc4fe524a
Danny4life <osiki.segun@gmail.com>
2023-10-18 22:09:15 +0100

    create employee model class

---
+++ src/main/java/com/osiki/springrestdocker/model/Employee.java
@@ -0,0 +1,18 @@
+package com.osiki.springrestdocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+}
