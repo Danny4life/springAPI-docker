9673fc03496c762c4843249a71059ff375f9630e
Danny4life <osiki.segun@gmail.com>
2023-10-18 22:33:51 +0100

    create employee repository class

--- src/main/java/com/osiki/springrestdocker/model/Employee.java
+++ src/main/java/com/osiki/springrestdocker/model/Employee.java
@@ -1,14 +1,12 @@
 package com.osiki.springrestdocker.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Employee {
 
     private Long id;
