8bee425f11ad26e39db4fadf30e5d523e906572f
Danny4life <osiki.segun@gmail.com>
2023-10-20 09:34:36 +0100

    create get employee by id method endpoint inside controller class

--- src/main/java/com/osiki/springrestdocker/controller/EmployeeController.java
+++ src/main/java/com/osiki/springrestdocker/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.osiki.springrestdocker.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,15 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    // get all employees
     @GetMapping
-    public ResponseEntity<List<Employee>> getEmployees(){
+    public ResponseEntity<List<Employee>> getAllEmployees(){
         return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    // get employee by id
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
     }
 }
