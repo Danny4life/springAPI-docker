2a07788d08b04ef8b07e9031ff875769c6d23d69
Danny4life <osiki.segun@gmail.com>
2023-10-20 09:28:03 +0100

    create employee controller class

---
+++ src/main/java/com/osiki/springrestdocker/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.osiki.springrestdocker.controller;
+
+import com.osiki.springrestdocker.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+}
