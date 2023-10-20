f3d2a9adf90e723918b204709384a59873b84f30
Danny4life <osiki.segun@gmail.com>
2023-10-20 09:31:34 +0100

    create get employees method endpoint inside controller class

--- src/main/java/com/osiki/springrestdocker/controller/EmployeeController.java
+++ src/main/java/com/osiki/springrestdocker/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.osiki.springrestdocker.controller;
 
+import com.osiki.springrestdocker.model.Employee;
 import com.osiki.springrestdocker.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getEmployees(){
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
 }
