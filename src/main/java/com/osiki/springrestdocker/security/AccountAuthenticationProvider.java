ba3dd2dcba34fb496d236ec38df9348844964178
Danny4life <osiki.segun@gmail.com>
2023-10-24 16:22:59 +0100

    create account authentication provider class

---
+++ src/main/java/com/osiki/springrestdocker/security/AccountAuthenticationProvider.java
@@ -0,0 +1,25 @@
+package com.osiki.springrestdocker.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@RequiredArgsConstructor
+public class AccountAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        return null;
+    }
+}
