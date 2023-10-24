48c62106ad85597286abede88d2b2d72ccfc2bb5
Danny4life <osiki.segun@gmail.com>
2023-10-24 12:47:06 +0100

    add logic for implementing create account

--- src/main/java/com/osiki/springrestdocker/service/impl/AccountServiceImpl.java
+++ src/main/java/com/osiki/springrestdocker/service/impl/AccountServiceImpl.java
@@ -1,21 +1,34 @@
 package com.osiki.springrestdocker.service.impl;
 
 import com.osiki.springrestdocker.model.Account;
+import com.osiki.springrestdocker.model.Role;
 import com.osiki.springrestdocker.repository.AccountRepository;
+import com.osiki.springrestdocker.repository.RoleRepository;
 import com.osiki.springrestdocker.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
 
     private final AccountRepository accountRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder encoder;
     @Override
     public Account createAccount(Account account) {
-        return null;
+        account.setPassword(encoder.encode(account.getPassword()));
+        Role role = roleRepository.findByName("ROLE_USER");
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        account.setRoles(roles);
+
+        return accountRepository.save(account);
     }
 
     @Override
